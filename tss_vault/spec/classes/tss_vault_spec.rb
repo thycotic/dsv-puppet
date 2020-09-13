6af495fc7434a39b659ba5a1994d96b2b2c6b820
John Poulin <john.m.poulin@gmail.com>
2020-09-13 18:06:48 -0400

    this function works, it's a miracle

--- tss_vault/spec/classes/tss_vault_spec.rb
+++ tss_vault/spec/classes/tss_vault_spec.rb
@@ -10,7 +10,7 @@ describe 'tss_vault' do
       it { is_expected.to compile }
 
       it do
-        is_expected.to contain_file('/tmp/jptest4')
+        is_expected.to contain_file('/tmp/jptest4').with_content('Meow: TEST')
       end
     end
   end

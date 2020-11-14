58382672a58394bd35077afca679cc3c9ecc72b5
Ricky White <ricky@whitelionmedia.com>
2020-11-14 11:11:41 -0500

    Changed the test class name to match the renamed init class

--- thycotic_secrets/spec/classes/thycotic_secrets_spec.rb
+++ thycotic_secrets/spec/classes/thycotic_secrets_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe 'thycotic_secret' do
+describe 'thycotic_secrets' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }

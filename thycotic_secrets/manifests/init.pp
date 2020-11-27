10bc68cf72dfb218c81e07745e359993ba32133d
Ricky White <ricky@whitelionmedia.com>
2020-11-27 09:54:25 -0500

    Rmeoved the mode attribute from the init class as now redundant.

--- thycotic_secrets/manifests/init.pp
+++ thycotic_secrets/manifests/init.pp
@@ -4,15 +4,10 @@
 # @param metadata
 #    Boolean value to determine if we want to return the metadata for a secret
 #
-# @param mode
-#    Determines whether we're accessing secrets for DSV or TSS
-#
 # @param storage_name
 #   Specifies the filename where the file will be installed (inside /tmp)
 
 class thycotic_secrets(
-  Enum['dsv', 'tss'] $mode = 'dsv',
   Boolean $metadata = false,
   String $storage_name = 'test-secret.txt',
-
 ) {}

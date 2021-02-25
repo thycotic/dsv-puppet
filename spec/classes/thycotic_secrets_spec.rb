794d868a0592f91b279ddb30879be406035848f2
Ricky White <ricky@whitelionmedia.com>
2021-02-25 11:03:59 -0500

    Fixed linting and formating errors on pdk validate

--- spec/classes/thycotic_secrets_spec.rb
+++ spec/classes/thycotic_secrets_spec.rb
@@ -3,19 +3,18 @@ require 'spec_helper'
 on_supported_os.each do |os, os_facts|
   describe 'thycotic_dsv::dsv' do
     context "for DSV on #{os}" do
-   
       let(:facts) { os_facts }
       let(:pre_condition) { 'include thycotic_dsv' }
-      
-      let(:params) { {
-        client_id: ENV["CLIENT_ID"],
-        client_secret: ENV["CLIENT_SECRET"],
-        tenant: ENV["TENANT"],
-        secret_path: ENV["SECRET_PATH"]
-      } }
+      let(:params) do
+        {
+          client_id: ENV['CLIENT_ID'],
+          client_secret: ENV['CLIENT_SECRET'],
+          tenant: ENV['TENANT'],
+          secret_path: ENV['SECRET_PATH'],
+        }
+      end
 
       it { is_expected.to compile }
-
     end
   end
 end
